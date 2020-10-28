5771cf4913b9de4ccc291b792d237b699203a3c3
khizerrocks.demon@gmail.com <ksyed@connexservice.ca>
2020-10-27 23:33:57 -0400

    Final Commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "some-key",
+  authDomain: "domain",
+  databaseURL: "database-url",
+  projectId: "project-id",
+  storageBucket: "bucket-name",
+  messagingSenderId: "messaging-sender-id"
+};
